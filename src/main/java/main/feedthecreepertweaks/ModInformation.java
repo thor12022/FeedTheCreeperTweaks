44f9feb2a975d0382536f27f9aeb43a209e392f3
thor12022 <thor12022@gmail.com>
2016-01-26 22:30:21 -0600

    Build 2.2.0

--- src/main/java/main/feedthecreepertweaks/ModInformation.java
+++ src/main/java/main/feedthecreepertweaks/ModInformation.java
@@ -10,8 +10,8 @@ public class ModInformation
    public static final String NAME = "Feed the Creeper Tweaks";
    public static final String ID = "feedthecreepertweaks";
    public static final String CHANNEL = "FeedTheCreeperTweaks";
-   public static final String DEPEND = "required-after:progressiveautomation;required-after:Metallurgy;required-after:TConstruct;after:ExtraTiC";
-   public static final String VERSION = "2.1.2";
+   public static final String DEPEND = "required-after:progressiveautomation;required-after:Metallurgy;required-after:metallurgycm;required-after:Metallurgy;required-after:TConstruct;after:ExtraTiC";
+   public static final String VERSION = "2.2.0";
    public static final String CLIENTPROXY = "main.feedthecreepertweaks.proxies.ClientProxy";
    public static final String COMMONPROXY = "main.feedthecreepertweaks.proxies.CommonProxy";;
 }
