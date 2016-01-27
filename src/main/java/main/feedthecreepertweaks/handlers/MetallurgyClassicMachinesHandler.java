0853e76231e6ed34cec8c6f167437964c990110c
thor12022 <thor12022@gmail.com>
2016-01-26 22:29:48 -0600

    Add recipes to Metallurgy Classic Machines Crusher
    Issue #5

--- src/main/java/main/feedthecreepertweaks/handlers/MetallurgyClassicMachinesHandler.java
+++ src/main/java/main/feedthecreepertweaks/handlers/MetallurgyClassicMachinesHandler.java
@@ -4,6 +4,7 @@ import com.teammetallurgy.metallurgycm.crafting.RecipesCrusher;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -14,9 +15,17 @@ public class MetallurgyClassicMachinesHandler
    
    public static void init(FMLInitializationEvent event)
    {
-      RecipesCrusher.addOreDicRecipe("stone", new ItemStack(Item.getItemFromBlock(Blocks.cobblestone)), 0.1f);
-      RecipesCrusher.addOreDicRecipe("cobblestone", new ItemStack(Item.getItemFromBlock(Blocks.gravel)), 0.1f);
-      RecipesCrusher.addOreDicRecipe("gravel", new ItemStack(Item.getItemFromBlock(Blocks.sand)), 0.1f);
+      RecipesCrusher.addOreDicRecipe("stone", new ItemStack(Item.getItemFromBlock(Blocks.cobblestone)), 0f);
+      RecipesCrusher.addOreDicRecipe("cobblestone", new ItemStack(Item.getItemFromBlock(Blocks.gravel)), 0f);
+      RecipesCrusher.addOreDicRecipe("gravel", new ItemStack(Item.getItemFromBlock(Blocks.sand)), 0f);
+      RecipesCrusher.addOreDicRecipe("clothWool", new ItemStack(Items.string,4), 0f);
+      RecipesCrusher.addOreDicRecipe("blockGlass", new ItemStack(Item.getItemFromBlock(Blocks.sand)), 0f);
+      RecipesCrusher.addRecipe(new ItemStack(Blocks.clay), new ItemStack(Items.clay_ball, 4), 0f);
+      RecipesCrusher.addRecipe(new ItemStack(Items.bone), new ItemStack(Items.dye, 6, 15), 0f);
+      RecipesCrusher.addOreDicRecipe("oreQuartz", new ItemStack(Items.quartz,4), 0.1f);
+      RecipesCrusher.addOreDicRecipe("oreDiamond", new ItemStack(Items.diamond,2), 0.1f);
+      RecipesCrusher.addOreDicRecipe("oreCoal", new ItemStack(Items.coal,4), 0.1f);
+      RecipesCrusher.addOreDicRecipe("itemBlazeRod", new ItemStack(Items.blaze_powder,4), 0f);
       
    }
    
